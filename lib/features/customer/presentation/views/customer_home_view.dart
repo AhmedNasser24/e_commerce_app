44ab6e089e9c8cc2ba9c7fecd91d2fbed7030f17
AhmedNasser24 <https://github.com/AhmedNasser24>
2024-08-16 15:30:25 +0300

    create customer home view

---
+++ lib/features/customer/presentation/views/customer_home_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/customer_home_body.dart';
+
+class CustomerHomeView extends StatefulWidget {
+  const CustomerHomeView({super.key});
+
+  @override
+  State<CustomerHomeView> createState() => _CustomerHomeViewState();
+}
+
+class _CustomerHomeViewState extends State<CustomerHomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: CustomerHomeBody(),
+    );
+  }
+}
