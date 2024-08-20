cad95036241d075ff4f2b01ff3dc9703fef3ecf7
AhmedNasser24 <https://github.com/AhmedNasser24>
2024-08-20 07:30:51 +0300

    fix issue with list of category

--- lib/core/functions/show_category_pop_up_menu.dart
+++ lib/core/functions/show_category_pop_up_menu.dart
@@ -2,9 +2,12 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 
+import '../../generated/l10n.dart';
 import 'list_of_product_category.dart';
 
 Widget showCategoryPopUpMenu(context) {
+  List < String > categoriesList = [S.of(context).all_category];
+  categoriesList.addAll(categoryList(context));
   return PopupMenuButton<String>(
     icon: const Icon(
       Icons.arrow_drop_down,
@@ -12,7 +15,7 @@ Widget showCategoryPopUpMenu(context) {
       size: 30,
     ),
     itemBuilder: (BuildContext context) {
-      return categoryList(context).map((String category) {
+      return categoriesList.map((String category) {
         return PopupMenuItem<String>(
           value: category,
           child: Text(category),
