1ea54dfcde8b94b8aee414dae581270628a7a441
AhmedNasser24 <https://github.com/AhmedNasser24>
2025-02-26 08:40:07 +0200

    add app icon

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -1,6 +1,11 @@
+// ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
   
+  /// Assets for imagesAppLogo
+  /// assets/images/app_logo.png
+  static const String imagesAppLogo = "assets/images/app_logo.png";
+
   /// Assets for imagesBag
   /// assets/images/bag.jpeg
   static const String imagesBag = "assets/images/bag.jpeg";
