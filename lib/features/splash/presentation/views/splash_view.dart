a4c85260be9918a1d30ec95059adeaab68eb726b
AhmedNasser24 <https://github.com/AhmedNasser24>
2025-02-26 09:05:32 +0200

    make splash view in portrait orientation only

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,8 +1,8 @@
 import 'package:e_commerce/core/utils/app_images.dart';
 import 'package:e_commerce/core/utils/app_style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gap/gap.dart';
-
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -13,6 +13,21 @@ class SplashView extends StatefulWidget {
 
 class _SplashViewState extends State<SplashView> {
   @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setPreferredOrientations(DeviceOrientation.values);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
@@ -21,11 +36,12 @@ class _SplashViewState extends State<SplashView> {
             children: [
               const Spacer(),
               Container(
-                constraints: const BoxConstraints(maxWidth: 300),
-                child: Image.asset(Assets.imagesAppLogo)),
+                  constraints: const BoxConstraints(maxWidth: 300),
+                  child: Image.asset(Assets.imagesAppLogo)),
               const Gap(20),
-              const Text("E Commmerce " , style:AppStyle.bold28),
-              const Text("for both Trader & Customer" , style:AppStyle.regular14),
+              const Text("E Commmerce ", style: AppStyle.bold28),
+              const Text("for both Trader & Customer",
+                  style: AppStyle.regular14),
               const Spacer(
                 flex: 2,
               ),
